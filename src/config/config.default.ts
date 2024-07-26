357330d0f0ccdcc2e6ebd8670931caa34ca2ebbf
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-07-27 00:05:03 +0300

    move influx token to config add autodeploy

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -2,6 +2,11 @@ const config = {
 	sentryDsn: "",
 	schemaRegistryHost: "http://gql-schema-registry:3000",
 	selfUrl: "telemetry-api:5000",
+
+	// you get this token from the InfluxDB UI after you spin up container
+	// and go through installation
+	// see github.com/gratheon/grafana docker-compose.yml
+	influxToken: ""
 };
 
 export default config;
--- src/models/influx.ts
+++ src/models/influx.ts
@@ -1,12 +1,13 @@
 //repl.repl.ignoreUndefined=true
 
 import { InfluxDB } from '@influxdata/influxdb-client'
+import config from '../config'
 
 let client
 
 export async function initInflux() {
 	if (!client) {
-		const token = process.env.INFLUXDB_TOKEN
+		const token = config.influxToken;
 		const port = process.env.INFLUXDB_PORT || 8086
 		const host = process.env.INFLUXDB_HOST || 'localhost'
 		const url = `http://${host}:${port}`
