eb06da78210f5a3c193b0cf96e3792718ce0f43c
Artjom Kurapov <artkurapov@gmail.com>
2024-09-07 05:05:44 +0300

    fix build

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
 cd /www/telemetry-api/
+rm -rf ./app
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml up --build -d
